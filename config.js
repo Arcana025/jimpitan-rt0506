1cb1afb0655e83776e0c9df056a8356418aa1834
Arcana025 <albynugrohofirdaus25@gmail.com>
2026-09-07 23:31:42 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Konfigurasi PWA Jimpitan RT 05 & 06
 // Ganti hanya URL di bawah ini dengan URL Web App Google Apps Script Anda.
 window.JIMPITAN_CONFIG = {
-  GAS_API_URL: 'PASTE_URL_WEB_APP_GAS_DI_SINI'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz2Ko4P36sRr4aNWfA68A-zAjvT_DGRUjYGg6AxgOFZVTO9oQjCsq3k6Yqy8ol5__pE/exec'
 };
