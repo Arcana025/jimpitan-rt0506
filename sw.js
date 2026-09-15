689c6acee64d5c24bbfb8879362c797c8d61fd52
Arcana025 <albynugrohofirdaus25@gmail.com>
2026-09-15 21:56:16 +0700

    Update CACHE_NAME to version 6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jimpitan-rt0506-pwa-v5';
+const CACHE_NAME = 'jimpitan-rt0506-pwa-v6';
 const APP_SHELL = [
   './', './index.html', './manifest.json', './config.js', './sw.js',
   './icons/icon-192.png', './icons/icon-512.png'
